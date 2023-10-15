8931c97d0068a41f54db3cbfa05eba2e00b09392
Christian Lee <christianjslee@gmail.com>
2023-10-15 14:44:43 -0400

    Add Vercel Analytics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  inject({ mode: dev ? 'development' : 'production' });
+</script>
+
+<slot />
+
+<style>
+	@import './reset.css';
+</style>
--- src/routes/__layout.svelte
+++
@@ -1,5 +0,0 @@
-<slot />
-
-<style>
-	@import './reset.css';
-</style>
